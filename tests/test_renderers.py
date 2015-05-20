49b132c7504d5941e7aedfabd3fd2a76323c2c75
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-05-20 16:39:01 +0200

    Further tests for renderers.

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -2,7 +2,8 @@
 
 from pyramid_urireferencer.models import (
     RegistryResponse,
-    ApplicationResponse
+    ApplicationResponse,
+    Item
 )
 
 class TestRenderers:
@@ -17,10 +18,30 @@ class TestRenderers:
         assert r['count'] == 0
         assert len(r['applications']) == 0
 
+    def test_registry_renderer_one_app_no_results(self):
+        ar = ApplicationResponse(
+            'My app',
+            'http://something.example.org',
+            'http://somethingelse.example.org',
+            True,
+            False,
+            0,
+            []
+        )
+        rr = RegistryResponse('http://id.example.org/foo/1', True, False, 0, [ar])
+        from pyramid_urireferencer.renderers import registry_adapter
+        r = registry_adapter(rr, {})
+        assert r['query_uri'] == 'http://id.example.org/foo/1'
+        assert r['success']
+        assert not r['has_references']
+        assert r['count'] == 0
+        assert len(r['applications']) == 1
+        assert 'title' in r['applications'][0]
+
     def test_empty_application_renderer(self):
         ar = ApplicationResponse(
             'My app',
-            'http://something.example.org', 
+            'http://something.example.org',
             'http://somethingelse.example.org',
             True,
             False,
@@ -35,3 +56,25 @@ class TestRenderers:
         assert not r['has_references']
         assert r['count'] == 0
         assert len(r['items']) == 0
+
+    def test_application_renderer_one_item(self):
+        ar = ApplicationResponse(
+            'My app',
+            'http://something.example.org',
+            'http://somethingelse.example.org',
+            True,
+            False,
+            0,
+            [Item('http://something.example.org/thingy/thing', 'My item')]
+        )
+        from pyramid_urireferencer.renderers import application_adapter
+        r = application_adapter(ar, {})
+        assert r['uri'] == 'http://something.example.org'
+        assert r['url'] == 'http://somethingelse.example.org'
+        assert r['success']
+        assert not r['has_references']
+        assert r['count'] == 0
+        assert len(r['items']) == 1
+        assert 'title' in r['items'][0]
+        assert 'uri' in r['items'][0]
+
