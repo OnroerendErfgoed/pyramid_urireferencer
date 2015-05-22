1ea8f23f7f2009f04e5a50c8041458fbad6792ce
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-05-22 15:04:29 +0200

    Some minor stuff.
    * Corrected tox config
    * Added an AbstractReferencer
    * Docstrings

--- pyramid_urireferencer/referencer.py
+++ pyramid_urireferencer/referencer.py
@@ -8,33 +8,60 @@ log = logging.getLogger(__name__)
 
 from .models import RegistryResponse
 
-
-class Referencer:
+class AbstractReferencer:
     """
-    Interface voor de referencesPlugin. De plugin staat in voor volgende zaken:
-    1) Nagaan of een uri uit een authentieke bron gebruikt wordt in de applicatie die de plugin inplugt.
-    2) Controleren of een uri uit de eigen applicatie gebruikt wordt in een andere applicatie door middel van het raadplegen van de registry.
+    This is an abstract class that defines what a Referencer needs to be able to handle.
+
+    It does two things:
+
+        * Check if a uri is being used in this application and report on this.
+        * Check if a certain uri is being used in another application by query
+          a central registry.
     """
     __metaclass__ = abc.ABCMeta
-
-    def __init__(self, registry_url, **kwargs):
-        '''Create a new referencer-object
-        '''
-        self.registry_url = registry_url
 
     @abc.abstractmethod
     def references(self, uri):
         """
-        Abstract method (to implement by the application that implements the plugin) to check if a specific uri is used in the application
-        :param: :class: String unique resource identifier (uri)
-        :rtype: :class: ApplicationResponse
+        This method checks if a certain uri is being referenced by any other
+        resource within this application.
+
+        :param string uri: URI of the resource we need to check for
+        :rtype: :class:`pyramid_urireferencer.models.ApplicationResponse`
         """
+
+    @abc.abstractmethod
+    def is_referenced(self, uri):
+        """
+        This method checks if a certain uri is being referenced from resources
+        in other applications.
+
+        :param string uri: URI of the resource that needs to be checked
+        :rtype: :class:`pyramid_urireferencer.models.RegistryResponse`
+        """
+
+
+class Referencer(AbstractReferencer):
+    """
+    This is an implementation of the :class:`AbstractReferencer` that adds a
+    generic :meth:`is_referenced` method and a plain :meth:`references` method..
+    """
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, registry_url, **kwargs):
+        '''
+        :param string registry_url: Locatie where the central registry can be
+            found
+        '''
+        self.registry_url = registry_url
 
     def is_referenced(self, uri):
         """
-        Method that the application can use to check if there are other applications known in the central registry that reference to the specific uri
-        :param: :class: String unique resource identifier (uri)
-        :rtype: :class: Response returns information about the applications that reference to the specific uri
+        This method checks if a certain uri is being referenced from resources
+        in other applications.
+
+        :param string uri: URI of the resource that needs to be checked
+        :rtype: :class:`pyramid_urireferencer.models.RegistryResponse`
         """
         try:
             url = self.registry_url + '/references?uri=' + uri
@@ -43,5 +70,3 @@ class Referencer:
         except Exception as e:
             log.error(e)
             return RegistryResponse(uri, False, None, None, None)
-
-
