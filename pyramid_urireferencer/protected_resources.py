cc097b00fea1884490ec0fab2a85e7d1d8fb4892
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-07-06 11:58:56 +0200

    Some doc updates.

--- pyramid_urireferencer/protected_resources.py
+++ pyramid_urireferencer/protected_resources.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
+'''
+Thids module is used when blocking operations on a certain uri
+that might be used in external applications.
+
+.. versionadded:: 0.4.0
+'''
+
 from pyramid.httpexceptions import (
     HTTPInternalServerError,
     HTTPConflict)
 
 import pyramid_urireferencer
 
-'''
-Thid module is used when blocking operations on a certain uri
-that might be used in external applications.
-'''
-
-
 
 def protected_operation(fn):
     '''
-    use this decorator to prevent an operation from being executed
-    when the related uri resource is still in use
-    '''
+    Use this decorator to prevent an operation from being executed
+    when the related uri resource is still in use.
 
+    :raises pyramid.httpexceptions.HTTPConflict: Signals that we don't want to 
+        delete a certain URI because it's still in use somewhere else.
+    :raises pyramid.httpexceptions.HTTPInternalServerError: Raised when we were
+        unable to check that the URI is no longer being used.
+    '''
     def advice(parent_object, *args, **kw):
         id = parent_object.request.matchdict['id']
         referencer = pyramid_urireferencer.get_referencer(parent_object.request.registry)
@@ -25,7 +30,7 @@ def protected_operation(fn):
         registery_response = referencer.is_referenced(uri)
         if not registery_response.success:
             raise HTTPInternalServerError(
-                detail="Urireferencer: Something goes wrong while retrieving references of the uri {0}".format(uri))
+                detail="Urireferencer: Something went wrong while retrieving references of the uri {0}".format(uri))
         if registery_response.has_references:
             raise HTTPConflict(
                 detail="Urireferencer: The uri {0} is still in use by other applications: {1}".
